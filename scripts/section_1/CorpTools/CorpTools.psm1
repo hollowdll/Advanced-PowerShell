ec9f8292f19e4bf2d30cec0d26bf7c4ca01d9529
hollowdll <pixerchat@gmail.com>
2023-01-27 11:27:04 +0200

    exercises 4 and 5

--- scripts/section_1/CorpTools/CorpTools.psm1
+++ scripts/section_1/CorpTools/CorpTools.psm1
@@ -1,13 +1,20 @@
 function Get-FileVersion {
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory=$true, HelpMessage="Enter a file name!")]
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   HelpMessage="Enter a file name!")]
         [ValidatePattern("exe$")]
         [Alias("ItemName")]
-        [string]$FileName
+        [string[]]$FileName
     )
 
-    Write-Verbose "Checking $FileName"
-    Get-ItemPropertyValue -Path $FileName -Name VersionInfo | Select-Object ProductVersion,
-    FileVersion, CompanyName, FileName
+    PROCESS {
+        foreach($File in $FileName) {
+            Write-Verbose "Checking $FileName"
+            Get-ItemPropertyValue -Path $File -Name VersionInfo | Select-Object ProductVersion,
+            FileVersion, CompanyName, FileName
+        }
+    }
 }
