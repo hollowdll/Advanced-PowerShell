1df5bdf6670a0f3be6f1f2f8abdc91247e151f8c
hollowdll <pixerchat@gmail.com>
2023-02-21 20:35:08 +0200

    post method

---
+++ REST_API_methods/add-new.ps1
@@ -0,0 +1,19 @@
+$RequestParameters = @{
+    URI    = "http://localhost:3000/api/resource"
+    Method = "POST"
+    Headers = @{
+        "Content-Type" = "application/json"
+    }
+    Body = @{
+        "name" = "Test";
+        "example" = "example";
+    } | ConvertTo-Json
+}
+
+$Response = Invoke-WebRequest @RequestParameters
+$StatusCode = $Response.StatusCode
+$StatusDescription = $Response.StatusDescription
+$PrettyJSON = $Response | ConvertFrom-Json | ConvertTo-Json -Depth 2
+
+Write-Host "Status Code: $StatusCode $StatusDescription"
+Write-Host $PrettyJSON
