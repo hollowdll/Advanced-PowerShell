110b9f80b3d3ef53c2a15dd393b7ac7dec1899aa
hollowdll <pixerchat@gmail.com>
2023-03-01 19:40:12 +0200

    finish JSONreport.ps1

--- section_5/JSONreport.ps1
+++ section_5/JSONreport.ps1
@@ -9,13 +9,18 @@ Param (
 )
 
 $inputdata = Get-Content $FileName | ConvertFrom-Json
-$outputdata = @{}
+$outputdata = @()
 
 foreach ($computer in $inputdata) {
     $computer = $computer | Select-Object Name
-    # add data to output 
+    $computer | Add-Member -Name OSInfo -Value (Get-OSInfo -ComputerName $computer.Name) -MemberType NoteProperty
+    $computer | Add-Member -Name MemInfo -Value (Get-MemInfo -ComputerName $computer.Name) -MemberType NoteProperty
+    $computer | Add-Member -Name DiskInfo -Value (Get-DiskInfo -ComputerName $computer.Name) -MemberType NoteProperty
+    $outputdata += $computer
 }
 
-$outputdata | ConvertTo-Json
+$outputJSON = $outputdata | ConvertTo-Json
+Write-Host $outputJSON
 
 # Write to file
+$outputJSON | Out-File -FilePath "inventory-output.json"
